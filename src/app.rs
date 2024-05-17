5706e7fd8529ebbd79d59e085e691e75a686a4ef
cd <cdominik@gmail.com>
2024-05-17 14:39:26 +0200

    MVP of spawning enemies

--- src/app.rs
+++ src/app.rs
@@ -1,41 +1,209 @@
+use egui::Color32;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+pub struct GameState {
+    excellency: Excellency,
+    enemies: Vec<Enemy>,
+    enemy_spawner: EnemySpawner,
 }
 
-impl Default for TemplateApp {
-    fn default() -> Self {
+#[derive(serde::Deserialize, serde::Serialize)]
+struct EnemySpawner {
+    timer: Timer,
+    maximum_hp: f32,
+    speed: f32,
+    damage: f32,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct Timer {
+    total: f32,
+    remaining: f32,
+    has_just_finished: bool,
+    one_shot: bool,
+    paused: bool,
+}
+
+impl Timer {
+    pub fn new(total: f32) -> Self {
         Self {
-            // Example stuff:
-            label: "Does this work ?!".to_owned(),
-            value: 2.7,
+            total,
+            remaining: total,
+            has_just_finished: false,
+            one_shot: false,
+            paused: false,
+        }
+    }
+
+    pub fn tick(&mut self, delta: f32) {
+        if !self.paused {
+            self.remaining -= delta;
+            if self.remaining <= 0. {
+                self.has_just_finished = true;
+                self.remaining += self.total; // Necessary because of the case when we aren't exactly 0
+                if self.one_shot {
+                    self.paused = true
+                }
+            } else {
+                self.has_just_finished = false
+            }
+        }
+    }
+
+    pub fn has_just_finished(&self) -> bool {
+        self.has_just_finished
+    }
+
+    // pub fn pause(&mut self) {
+    //     self.paused = true
+    // }
+
+    // pub fn unpause(&mut self) {
+    //     self.paused = false
+    // }
+}
+
+impl GameState {
+    pub fn tick(&mut self, ctx: &egui::Context) {
+        let delta = ctx.input(|i| i.stable_dt);
+
+        let mut enemies = vec![];
+
+        for enemy in self.enemies.iter_mut() {
+            match enemy.tick(delta) {
+                EnemyAfterTick::Normal => enemies.push(enemy.clone()), // TODO: Clone isn't strictly necessary here
+                EnemyAfterTick::ReachedExcellency => self.excellency.hp.take_damage(enemy.damage),
+            }
+        }
+
+        self.enemies = enemies;
+
+        self.enemy_spawner.timer.tick(delta);
+        if self.enemy_spawner.timer.has_just_finished() {
+            self.enemies.push(Enemy {
+                hp: HitPoints::new_full(self.enemy_spawner.maximum_hp),
+                damage: self.enemy_spawner.damage,
+                speed: self.enemy_spawner.speed,
+                distance: Distance::start(),
+            })
+        }
+
+        self.enemies.sort_by(|a, b| {
+            a.distance
+                .partial_cmp(&b.distance)
+                .expect("Compared two f32's")
+        })
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct Excellency {
+    hp: HitPoints,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
+struct Enemy {
+    hp: HitPoints,
+    damage: f32,
+    speed: f32,
+    distance: Distance,
+}
+
+impl Enemy {
+    pub fn tick(&mut self, delta: f32) -> EnemyAfterTick {
+        self.distance.0 -= delta * self.speed;
+        match self.distance.0 > 0. {
+            true => EnemyAfterTick::Normal,
+            false => EnemyAfterTick::ReachedExcellency,
         }
     }
 }
 
-impl TemplateApp {
+enum EnemyAfterTick {
+    Normal,
+    ReachedExcellency,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, PartialEq, PartialOrd, Clone)]
+struct Distance(f32);
+
+impl Distance {
+    pub fn start() -> Self {
+        Self(100.)
+    }
+
+    pub fn as_progress_bar(&self) -> egui::ProgressBar {
+        egui::ProgressBar::new(self.0 / 100.).show_percentage()
+    }
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Clone)]
+struct HitPoints {
+    maximum: f32,
+    current: f32,
+}
+
+impl HitPoints {
+    pub fn new_full(maximum: f32) -> Self {
+        Self {
+            maximum,
+            current: maximum,
+        }
+    }
+
+    pub fn as_progress_bar(&self) -> egui::ProgressBar {
+        egui::ProgressBar::new(self.current / self.maximum)
+            .text(format!("{}/{}", self.current, self.maximum))
+            .fill(Color32::DARK_RED)
+    }
+
+    pub fn take_damage(&mut self, damage: f32) {
+        self.current -= damage;
+    }
+
+    pub fn reset(&mut self) {
+        self.current = self.maximum;
+    }
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            excellency: Excellency {
+                hp: HitPoints::new_full(100.),
+            },
+            enemy_spawner: EnemySpawner {
+                timer: Timer::new(1.),
+                maximum_hp: 10.,
+                speed: 5.,
+                damage: 2.,
+            },
+            enemies: vec![],
+        }
+    }
+}
+
+impl GameState {
     /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+
+        // TODO: Commented out persistence for now
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
 
         Default::default()
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for GameState {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -46,64 +214,58 @@ impl eframe::App for TemplateApp {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
+        self.tick(ctx);
 
-            egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_dark_light_mode_buttons(ui);
+        egui::SidePanel::right("right_panel").show(ctx, |ui| {
+            ui.heading("Enemy spawner");
+            ui.horizontal(|ui| {
+                ui.label("Damage:");
+                ui.add(egui::Slider::new(&mut self.enemy_spawner.damage, 0. ..=20.));
             });
+            ui.horizontal(|ui| {
+                ui.label("Maximum HP:");
+                ui.add(egui::Slider::new(
+                    &mut self.enemy_spawner.maximum_hp,
+                    0. ..=100.,
+                ));
+            });
+            ui.horizontal(|ui| {
+                ui.label("Speed:");
+                ui.add(egui::Slider::new(&mut self.enemy_spawner.speed, 0. ..=20.));
+            });
+
+            ui.separator();
+            ui.heading("Enemies");
+
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for enemy in self.enemies.iter() {
+                    ui.horizontal(|ui| {
+                        ui.label("Distance:");
+                        ui.add(enemy.distance.as_progress_bar());
+                    });
+                    ui.horizontal(|ui| {
+                        ui.label("HP:");
+                        ui.add(enemy.hp.as_progress_bar());
+                    });
+                    ui.label(format!("Damage: {}", enemy.damage));
+                    ui.label(format!("Speed: {}", enemy.speed));
+                    ui.separator();
+                }
+            })
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
+            ui.heading("Player stuff");
             ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
+            ui.horizontal(|ui| {
+                ui.label("HP:");
+                ui.add(self.excellency.hp.as_progress_bar());
             });
+            if ui.button("Reset HP").clicked() {
+                self.excellency.hp.reset()
+            }
         });
-    }
-}
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
+        ctx.request_repaint_after(std::time::Duration::from_millis(16)) // ~60fps
+    }
 }
